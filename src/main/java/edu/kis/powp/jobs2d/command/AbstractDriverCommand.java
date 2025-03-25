53c68697fd432848dab99b4c89b67f68715ca412
Paweł Hase <pawelhase@gmail.com>
2025-03-25 20:11:07 +0100

    Dodanie testów Zawierających figury rysowane z pomocą wzorca command

--- src/main/java/edu/kis/powp/jobs2d/command/AbstractDriverCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/AbstractDriverCommand.java
@@ -1,8 +1,6 @@
 package edu.kis.powp.jobs2d.command;
 
-import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverCommand;
-import edu.kis.powp.jobs2d.drivers.adapter.Job2dtoDrawPanelAdapter;
 
 public abstract class AbstractDriverCommand implements DriverCommand {
     protected int x;
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,9 +2,12 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
 
+import edu.kis.powp.jobs2d.FigureCommand;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+import edu.kis.powp.jobs2d.patterns.FigureCommandPatterns;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
@@ -14,13 +17,16 @@ public class SelectTestFigureOptionListener implements ActionListener {
 		this.driverManager = driverManager;
 	}
 
+	private final Map<FigureCommand, Runnable> actionMap = Map.of(
+			FigureCommand.FIGURE_JOE_1, () -> FiguresJoe.figureScript1(driverManager.getCurrentDriver()),
+			FigureCommand.FIGURE_JOE_2, () -> FiguresJoe.figureScript2(driverManager.getCurrentDriver()),
+			FigureCommand.FIGURE_COMMAND_SQ, () -> FigureCommandPatterns.figureSquare(driverManager.getCurrentDriver()),
+			FigureCommand.FIGURE_COMMAND_X, () -> FigureCommandPatterns.figureX(driverManager.getCurrentDriver())
+	);
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("Figure Joe 1")) {
-			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-		} else {
-			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-		}
-
-	}
+		FigureCommand figureCommand = FigureCommand.fromTitle(e.getActionCommand());
+        actionMap.get(figureCommand).run();
+    }
 }
