15957ab18248ed93c444dabb2dc85dfcf7cdfd6f
Paweł Hase <pawelhase@gmail.com>
2025-03-25 18:46:56 +0100

    Implementacja klass SetPositionCommand oraz OperateToCommand

--- src/main/java/edu/kis/powp/jobs2d/command/AbstractDriverCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/command/AbstractDriverCommand.java
@@ -7,21 +7,9 @@ import edu.kis.powp.jobs2d.drivers.adapter.Job2dtoDrawPanelAdapter;
 public abstract class AbstractDriverCommand implements DriverCommand {
     protected int x;
     protected int y;
-    private Job2dDriver driver;
 
     public AbstractDriverCommand(int x, int y) {
         this.x = x;
         this.y = y;
-        this.driver = new Job2dDriver() {
-            @Override
-            public void setPosition(int i, int i1) {
-
-            }
-
-            @Override
-            public void operateTo(int i, int i1) {
-
-            }
-        }
     }
 }
